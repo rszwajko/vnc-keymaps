71a35b1cbcc243b27250126f4b06d95e9db041ca
Radoslaw Szwajkowski <rszwajko@redhat.com>
2025-11-13 23:30:26 +0100

    Migrate from yarn classic to npm
    Get support for configuration from package.json i.e. publishConfig and
    packageManager properties.
    
    Enable ignore-scripts flag at the project level via .npmrc.
    
    Split eslint and prettier configuration. Fix formatting errors.
    
    Signed-off-by: Radoslaw Szwajkowski <rszwajko@redhat.com>

--- eslint.config.js
+++ eslint.config.js
@@ -2,11 +2,14 @@ import js from '@eslint/js';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 import { defineConfig } from 'eslint/config';
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import { fileURLToPath, URL } from 'node:url';
+import { includeIgnoreFile } from '@eslint/compat';
+
+const gitignorePath = fileURLToPath(new URL('.gitignore', import.meta.url));
 
 export default defineConfig([
-  { files: ['**/*.{js,mjs,cjs,ts,mts,cts}'], plugins: { js }, extends: ['js/recommended'] },
-  { files: ['**/*.{js,mjs,cjs,ts,mts,cts}'], languageOptions: { globals: globals.browser } },
-  tseslint.configs.recommended,
-  eslintPluginPrettierRecommended,
+  { files: ['**/*.{js,mjs,cjs,ts,mts,cts}'], plugins: { js }, extends: [js.configs.recommended] },
+  { files: ['**/*.{ts,mts,cts}'], plugins: { tseslint }, extends: [tseslint.configs.recommended] },
+  { files: ['src/**/*.{js,mjs,cjs,ts,mts,cts}'], languageOptions: { globals: globals.browser } },
+  includeIgnoreFile(gitignorePath, '.gitignore'),
 ]);
