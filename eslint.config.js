848f09547b909b228b7476261d731175eb2731d6
Radoslaw Szwajkowski <rszwajko@redhat.com>
2025-11-19 16:14:11 +0100

    Simplify the eslint setup
    Signed-off-by: Radoslaw Szwajkowski <rszwajko@redhat.com>

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,6 @@
 import js from '@eslint/js';
-import globals from 'globals';
 import tseslint from 'typescript-eslint';
+import prettierConfig from 'eslint-config-prettier';
 import { defineConfig } from 'eslint/config';
 import { fileURLToPath, URL } from 'node:url';
 import { includeIgnoreFile } from '@eslint/compat';
@@ -8,8 +8,8 @@ import { includeIgnoreFile } from '@eslint/compat';
 const gitignorePath = fileURLToPath(new URL('.gitignore', import.meta.url));
 
 export default defineConfig([
-  { files: ['**/*.{js,mjs,cjs,ts,mts,cts}'], plugins: { js }, extends: [js.configs.recommended] },
-  { files: ['**/*.{ts,mts,cts}'], plugins: { tseslint }, extends: [tseslint.configs.recommended] },
-  { files: ['src/**/*.{js,mjs,cjs,ts,mts,cts}'], languageOptions: { globals: globals.browser } },
+  js.configs.recommended,
+  tseslint.configs.recommended,
+  prettierConfig,
   includeIgnoreFile(gitignorePath, '.gitignore'),
 ]);
